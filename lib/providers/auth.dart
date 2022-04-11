b358539bc94f7eaf50c47f8c42ffda5fc0b10c17
gohilnilesh <gohilnilesh44472@gmail.com>
2022-04-11 17:09:29 +0530

    commit

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,10 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
-import 'dart:html';
-import 'dart:math';
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
-import 'dart:convert';
 import '../models/http_exception.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
